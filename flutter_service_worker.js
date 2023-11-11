06f1a23b871a803d7889aa725f6299e093c7b4df
Aditya Pandey <adityapandeyz@outlook.com>
2023-11-11 16:48:36 +0530

    web_commit_3

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -52,8 +52,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "f6af4405a2fce1bae3ec635799e8a50e
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "6f75d7f3367c99dd6278d7cf77b45a9b",
-"/": "6f75d7f3367c99dd6278d7cf77b45a9b",
+"index.html": "8f8894fa1164722f821087077257be03",
+"/": "8f8894fa1164722f821087077257be03",
 "main.dart.js": "200736f820529d43e889d89b1e8751cc",
 "manifest.json": "3fa2a27fae6779750786e760b43c3d07",
 "version.json": "b65d66bd3abefc03558f65f64639006c"};
